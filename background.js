b176b49c41661b968223d3c5d3e506984f581c6c
Simon POLET <32013966+Fogbose@users.noreply.github.com>
2024-05-31 13:27:46 +0200

    Use of Youtube Data API v3 to send a dislike

--- background.js
+++ background.js
@@ -1,35 +1,58 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action == 'notInterested') {
     const videoId = request.videoId;
-    sendYouTubeSignal(videoId, request.type);
+
+    chrome.identity.getAuthToken({ interactive: true }, (token) => {
+      const headers = new Headers();
+      headers.append('Authorization', `Bearer ${token}`);
+      headers.append('Content-type', 'application/json');
+
+      sendYTSignal(videoId, request.type, headers);
+    });
   }
 });
 
-function sendYouTubeSignal(videoId, type) {
+function sendYTSignal(videoId, type, headers) {
   if (type === 'video') {
-    sendNotInterestedSignal(videoId);
-    sendDontRecommendChannelSignal(videoId);
-    sendDislikeSignal(videoId);
-    sendRemoveFromHistorySignal(videoId);
+    sendNotInterestedSignal(videoId, headers);
+    sendDontRecommendChannelSignal(videoId, headers);
+    sendDislikeSignal(videoId, headers);
+    sendRemoveFromHistorySignal(videoId, headers);
   } else if (type === 'recommendation') {
-    sendNotInterestedSignal(videoId);
-    sendDontRecommendChannelSignal(videoId);
-    sendDislikeSignal(videoId);
+    sendNotInterestedSignal(videoId, headers);
+    sendDontRecommendChannelSignal(videoId, headers);
+    sendDislikeSignal(videoId, headers);
   }
 }
 
-function sendNotInterestedSignal(videoId) {
+function sendYTRequest(url, headers) {
+  fetch(url, {
+    method: 'POST',
+    headers: headers,
+  })
+    .then((response) => {
+      // DEPRECATED MUST TELL CONTENT.JS THAT IT'S DONE TO UPDATE FRONTEND
+      console.log(response);
+    })
+    .catch((error) => console.error('Error:', error));
+}
+
+function sendNotInterestedSignal(videoId, headers) {
   console.log(`Sending 'Not Interested' signal for video ${videoId}`);
 }
 
-function sendDontRecommendChannelSignal(videoId) {
+function sendDontRecommendChannelSignal(videoId, headers) {
   console.log(`Sending 'Don't Recommend Channel' signal for video ${videoId}`);
 }
 
-function sendDislikeSignal(videoId) {
+function sendDislikeSignal(videoId, headers) {
+  sendYTRequest(
+    `https://www.googleapis.com/youtube/v3/videos/rate?id=${videoId}&rating=dislike`,
+    headers
+  );
   console.log(`Sending 'Dislike' signal for video ${videoId}`);
 }
 
-function sendRemoveFromHistorySignal(videoId) {
+function sendRemoveFromHistorySignal(videoId, headers) {
   console.log(`Sending 'Remove From History' signal for video ${videoId}`);
 }
